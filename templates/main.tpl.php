eebfb23473aa3f3703f5f3542d6e225048ea9a30
i_smirnov <i_smirnov@mti.edu.ru>
2014-10-14 17:28:02 +0400

    fixed

--- templates/main.tpl.php
+++ templates/main.tpl.php
@@ -8,32 +8,52 @@
 	<link rel="stylesheet" href="css/style.css">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/calendar.js"></script>
 </head>
 <body>
+
 <div class="container">
-	<div class="row">
-		<div class="span4">
-			<!--Sidebar content-->werwwerewrwere
+	<div class="row" id="header">
+		<div class="span12">
+			Header
 		</div>
-		<div class="span8">
+	</div>
+	<div class="row" id="content">
+		<div class="span3">
+			Календарь
+
+			<table id="calendar2">
+				<thead>
+				<tr><td>‹<td colspan="5"><td>›
+				<tr><td>Пн<td>Вт<td>Ср<td>Чт<td>Пт<td>Сб<td>Вс
+				</thead>
+						<tbody>		</tbody>
+			</table>
+		</div>
+		<div class="span9">
 			<table class="table">
-				<?php foreach($TPL->table as $row): ?>
+				<?php foreach ($TPL->table as $row): ?>
 					<tr>
 						<td>
-							<?=$row['sign'] ?>
+							<?= $row['sign'] ?>
 						</td>
 						<td>
-							<img src="/images/<?=$row['card']?>_icon.jpg" >
+							<img src="/images/<?= $row['card'] ?>_icon.jpg">
 						</td>
 						<td>
-							<?=$row['sum'] ?>
+							<?= $row['sum'] ?>
 						</td>
 						<td>
-							<?=$row['item'] ?>
+							<?= $row['item'] ?>
 						</td>
 					</tr>
 				<?php endforeach; ?>
 			</table>
+		</div>
+	</div>
+	<div class="row" id="footer">
+		<div class="span12" >
+			футер
 		</div>
 	</div>
 </div>
