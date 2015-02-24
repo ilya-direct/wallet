caff637e6cf199e258f7022a2fd2aabbe469d838
Mifotake <ilya-direct@ya.ru>
2015-02-24 19:06:39 +0300

    fixed

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <div id="first_div"></div>
 <div>
 	<ul>
-		<li>Ссылка 1</li>
+		<li><a href="http://ya.ru">Ссылка 1</a></li>
 		<li>Ссылка 2</li>
 		<li>Ссылка 3</li>
 		<li>Ссылка 4</li>
@@ -32,15 +32,22 @@ YUI.add('module1',function(Y){
 	 //this.set_hello();
 },'0.0.1',{requires:['node']});
 YUI.add('module2',function(Y){
-	 Y.set_hello_in_div=function(){
+	var m2=Y.namespace('module2');
+	m2.set_hello_in_div=function(){
 		 var node = Y.one('#first_div');
 		 node.setHTML("<p>Hello World 2!</p>");
 	 }
 	 //this.set_hello();
 },'0.0.1',{requires:['node']});
-YUI().use('module2','module1',function(Y,msg){
+YUI().use('module1','module2',function(Y,msg){
 	console.log(msg);
 	Y.set_hello_in_div();
+});
+YUI().use('event',function(Y){
+	var link=Y.one('a');
+	link.on('click',function(e){
+		e.preventDefault();
+	});
 });
 </script>
 
